f563afe39e3e9d514ddebbb358ec33f6932643de
tslarkin <tsl1@cornell.edu>
2025-05-05 20:06:04 -0400

    page.html to display an image.

--- CFMulks/Notebooks/views.py
+++ CFMulks/Notebooks/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import Notebook, Page
 
-# Create your views here.
+def pages(request):
+    all = Page.objects.all().order_by('name')
+    paginator = Paginator(all, 1)
+    page_num = int(request.GET.get('page', 1))
+    page_num = max(page_num, 1)
+    page_num = min(page_num, paginator.num_pages)
+    page = paginator.page(page_num)
+    data = {
+        'page': page.object_list[0],
+        'page_num': page.number,
+    }
+    return render(request, 'page.html', data)
