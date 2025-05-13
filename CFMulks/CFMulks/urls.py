c42ea2bdac45cb2efef3c097cbd8d98b2a1a822d
Timothy Larkin <tsl1@cornell.edu>
2025-05-13 18:29:18 -0400

    Created login page. Reset superuser password.

--- CFMulks/CFMulks/urls.py
+++ CFMulks/CFMulks/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,6 +23,7 @@ from Notebooks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('scan/', views.ScanListView.as_view(), name='scan'),
     path("", views.ScanListView.as_view(), name='main')
 ] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
