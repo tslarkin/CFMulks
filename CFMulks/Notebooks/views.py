969029e2fd437cb98bb64b84fe741687a7618fbe
Timothy Larkin <tsl1@cornell.edu>
2025-05-13 18:00:37 -0400

    Fixed login required.

--- CFMulks/Notebooks/views.py
+++ CFMulks/Notebooks/views.py
@@ -3,7 +3,7 @@ from django.core.paginator import Paginator
 from .models import Notebook, Scan
 from django.views.generic import ListView
 from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def pages(request):
@@ -19,8 +19,7 @@ def pages(request):
     }
     return render(request, 'page.html', data)
 
-@login_required
-class ScanListView(ListView):
+class ScanListView(LoginRequiredMixin, ListView):
     paginate_by = 5
     model = Scan
 
