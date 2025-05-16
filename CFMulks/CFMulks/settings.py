0d39aa4c7e6be44f3ab89da727caf06bc794f4c9
Timothy Larkin <tsl1@cornell.edu>
2025-05-16 11:16:40 -0400

    Misspelled Compressed

--- CFMulks/CFMulks/settings.py
+++ CFMulks/CFMulks/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS =  [ BASE_DIR / 'static', ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE='whitenoise.storage.CampressedManifestStaticFilesStorage'
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
