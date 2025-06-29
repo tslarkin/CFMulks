d70ef641e3506339159abebd6c985b1570b1778c
Timothy Larkin <tsl1@cornell.edu>
2025-06-29 06:52:32 -0400

    Put DEBUG in env. Change base.html to allow local styles for a single page. Change home.html to use previous. Change layout of home.html to be center aligned in a div with max-width.

--- CFMulks/CFMulks/settings.py
+++ CFMulks/CFMulks/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['cfmulks.org','127.0.0.1', 'cfmulks-production.up.railway.app','https://cfmulks-production.up.railway.app', ]
 CSRF_TRUSTED_ORIGINS = ['https://cfmulks.org', 'https://cfmulks-production.up.railway.app',]
